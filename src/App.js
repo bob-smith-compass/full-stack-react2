479520956e6e41abfed040f773a3d85b3241e2a8
David <david.shams@gmail.com>
2019-09-14 16:19:49 -0500

    Routing render function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 // import Dashboard from './components/dashboard/Dashboard';
 import ConnectedDashboard from './components/dashboard/Dashboard';
+import Group from './components/group/Group';
 import { store } from './store/index.js';
 import { Provider } from 'react-redux';
 import { Router, Route } from 'react-router-dom';
@@ -12,12 +13,13 @@ function App() {
   return (
     <div>
       <Router history={history}>
-      <Provider store={store}>
-        {/* <Dashboard store={store} /> */}
-        {/* <Dashboard /> */}
-        <ConnectedDashboard />
-        <ConnectedDashboard />
-      </Provider>
+        <Provider store={store}>
+          {/* <Dashboard store={store} /> */}
+          {/* <Dashboard /> */}
+          <Route exact path="/" render={() => (<h3>Home</h3>)} />
+          <Route exact path="/groups" render={() => (<ConnectedDashboard />)} />
+          {/* <Route exact path="/groups" render={() => (<Group />)} /> */}
+        </Provider>
       </Router>
     </div>
   );
