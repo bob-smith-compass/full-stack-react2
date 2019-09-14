dc0908b9f028ee09be241c94721220fcf62eee59
David <david.shams@gmail.com>
2019-09-14 11:42:07 -0500

    Viala! Connected Redux Store through <Provider>

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div>
       <Provider store={store}>
         {/* <Dashboard store={store} /> */}
-        <Dashboard store={store} />
+        <Dashboard />
       </Provider>
     </div>
   );
--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 // import { store } from '../../store/index';
+import { connect } from 'react-redux';
 
-const Dashboard = (props) => {
+const Dashboard = ({groups}) => {
     
     return (
         <div>
             <h3>Dashboard</h3>
             {/* {JSON.stringify(props.store)} */}
             {/* {JSON.stringify(store.getState())} */}
-            {JSON.stringify(props.store.getState())}
+            {/* {JSON.stringify(props.store.getState())} */}
+            {JSON.stringify(groups)}
         </div>
     );
 }
 
-export default Dashboard;
+const ConnectedDashboard = connect(mapStateToProps)(Dashboard);
+export default ConnectedDashboard;
+
+function mapStateToProps(state){
+    /**
+     * whatever mapStateToProps returns
+     * becomes props of this component
+     */
+    return {
+        groups: state.groups
+    }
+}
